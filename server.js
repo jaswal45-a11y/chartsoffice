2ab66beb1678fbf8b9e3b1f7e956406a684946fd
Abhishek Jaswal <jaswal.45@gmail.com>
2026-08-31 23:48:21 +0530

    screener adding issue

--- server.js
+++ server.js
@@ -2595,7 +2595,12 @@ const server = http.createServer(async (req, res) => {
     const ext = path.extname(resolvedPath).toLowerCase();
     const contentType = MIME_TYPES[ext] || 'application/octet-stream';
 
-    res.writeHead(200, { 'Content-Type': contentType });
+    res.writeHead(200, {
+      'Content-Type': contentType,
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    });
     fs.createReadStream(resolvedPath).pipe(res);
   });
 });
